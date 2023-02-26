c07e4ff48595cb79941963c5d6ae7073b9a60eb9
MPM <125613349+mpm1299m@users.noreply.github.com>
2023-02-26 13:51:36 +0330

    luapay classbase

---
+++ config_lua/urls.py
@@ -0,0 +1,24 @@
+"""config_lua URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pages.views import IndexPage,About_US_Page
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("index/",IndexPage.as_view()),
+    path("aboutus/",About_US_Page.as_view()),
+]
