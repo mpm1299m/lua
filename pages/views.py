c7165c0fa60a9897c437636149885fd40edeec2c
MPM <125613349+mpm1299m@users.noreply.github.com>
2023-02-26 13:53:47 +0330

    luapay app

---
+++ pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+from django.template.response import TemplateResponse
+
+class IndexPage(generic.TemplateView):
+    template_name = ""
+class About_US_Page(generic.TemplateView):
+    template_name = ""
+
+
+
+
